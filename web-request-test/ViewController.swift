9454cdc4792fc0cb726ae9379ca3854479e03d48
Vyacheslav Horbach <vjacheslav.horbach@gmail.com>
2016-03-10 14:54:29 +0000

    learned how to use WKWebView

--- web-request-test/ViewController.swift
+++ web-request-test/ViewController.swift
@@ -7,12 +7,26 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var container: UIView!
+    var webView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        webView = WKWebView()
+        container.addSubview(webView)
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        let frame = CGRectMake(0, 0, container.bounds.width, container.bounds.height)
+        webView.frame = frame
+        
+        loadRequest("https://developer.apple.com/swift/blog/")
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +34,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func loadRequest(urlStr: String) {
+        let url = NSURL(string: urlStr)!
+        let request = NSURLRequest(URL: url)
+        
+        webView.loadRequest(request)
+    }
+    
+    @IBAction func onPressedShowSwift(sender: AnyObject) {
+        loadRequest("https://developer.apple.com/swift/blog/")
+    }
 
+    @IBAction func onPressedShowGossip(sender: AnyObject) {
+        loadRequest("http://www.tmz.com/")
+    }
+    
+    @IBAction func onPressedShowSteak(sender: AnyObject) {
+        loadRequest("https://en.wikipedia.org/wiki/Steak")
+    }
+    
 }
 
